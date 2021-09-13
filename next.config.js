07e7879f653fea9e07f1b1a670a58d4321aaee1e
Noprop <12greg21@gmail.com>
2021-09-12 21:03:04 -0400

    sending request to NASA API for current picture of the day

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    NASA: process.env.NEXT_PUBLIC_NASA,
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import {
   Layout,
   Page,
@@ -6,51 +6,23 @@ import {
   Card,
   Link,
   Button,
-  FormLayout,
   TextField,
-  AccountConnection,
   ChoiceList,
   SettingToggle,
 } from '@shopify/polaris';
 import {ImportMinor} from '@shopify/polaris-icons';
 
 export default function App() {
-  const [first, setFirst] = useState('');
-  const [last, setLast] = useState('');
-  const [email, setEmail] = useState('');
-  const [checkboxes, setCheckboxes] = useState([]);
-  const [connected, setConnected] = useState(false);
-
-  const handleFirstChange = useCallback((value) => setFirst(value), []);
-  const handleLastChange = useCallback((value) => setLast(value), []);
-  const handleEmailChange = useCallback((value) => setEmail(value), []);
-  const handleCheckboxesChange = useCallback(
-    (value) => setCheckboxes(value),
-    [],
-  );
-
-  const toggleConnection = useCallback(() => {
-    setConnected(!connected);
-  }, [connected]);
-
+  const API_KEY = useRef(process.env.NASA);
   const breadcrumbs = [{content: 'Sample apps'}, {content: 'next.js'}];
   const primaryAction = {content: 'New product'};
   const secondaryActions = [{content: 'Import', icon: ImportMinor}];
 
-  const choiceListItems = [
-    {label: 'I accept the Terms of Service', value: 'false'},
-    {label: 'I consent to receiving emails', value: 'false2'},
-  ];
-
-  const accountSectionDescription = connected
-    ? 'Disconnect your account from your Shopify store.'
-    : 'Connect your account to your Shopify store.';
-
-  const accountMarkup = connected ? (
-    <DisconnectAccount onAction={toggleConnection} />
-  ) : (
-    <ConnectAccount onAction={toggleConnection} />
-  );
+  useEffect(() => {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY.current}`)
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, [])
 
   return (
     <Page
@@ -60,69 +32,7 @@ export default function App() {
       secondaryActions={secondaryActions}
     >
       <Layout>
-        <Layout.AnnotatedSection
-          title="Style"
-          description="Customize the style of your checkout"
-        >
-          <SettingToggle
-            action={{
-              content: 'Customize Checkout',
-            }}
-          >
-            Upload your store’s logo, change colors and fonts, and more.
-          </SettingToggle>
-        </Layout.AnnotatedSection>
-
-        <Layout.AnnotatedSection
-          title="Account"
-          description={accountSectionDescription}
-        >
-          {accountMarkup}
-        </Layout.AnnotatedSection>
-
-        <Layout.AnnotatedSection
-          title="Form"
-          description="A sample form using Polaris components."
-        >
-          <Card sectioned>
-            <FormLayout>
-              <FormLayout.Group>
-                <TextField
-                  value={first}
-                  label="First name"
-                  placeholder="Tom"
-                  onChange={handleFirstChange}
-                  autoComplete="given-name"
-                />
-                <TextField
-                  value={last}
-                  label="Last name"
-                  placeholder="Ford"
-                  onChange={handleLastChange}
-                  autoComplete="family-name"
-                />
-              </FormLayout.Group>
-
-              <TextField
-                value={email}
-                label="Email"
-                placeholder="example@email.com"
-                onChange={handleEmailChange}
-                autoComplete="email"
-              />
-
-              <ChoiceList
-                allowMultiple
-                choices={choiceListItems}
-                selected={checkboxes}
-                onChange={handleCheckboxesChange}
-              />
-
-              <Button primary>Submit</Button>
-            </FormLayout>
-          </Card>
-        </Layout.AnnotatedSection>
-
+        
         <Layout.Section>
           <FooterHelp>
             For more details on Polaris, visit our{' '}
@@ -131,34 +41,6 @@ export default function App() {
         </Layout.Section>
       </Layout>
     </Page>
-  );
-}
-
-function ConnectAccount({onAction}) {
-  return (
-    <AccountConnection
-      action={{content: 'Connect', onAction}}
-      details="No account connected"
-      termsOfService={
-        <p>
-          By clicking Connect, you are accepting Sample’s{' '}
-          <Link url="https://polaris.shopify.com">Terms and Conditions</Link>,
-          including a commission rate of 15% on sales.
-        </p>
-      }
-    />
-  );
-}
-
-function DisconnectAccount({onAction}) {
-  return (
-    <AccountConnection
-      connected
-      action={{content: 'Disconnect', onAction}}
-      accountName="Tom Ford"
-      title={<Link url="http://google.com">Tom Ford</Link>}
-      details="Account id: d587647ae4"
-    />
   );
 }
 
